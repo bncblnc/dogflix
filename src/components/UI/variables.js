e982fe4e83b84a1eea80074c348f629790f003cb
bncblnc <bncblnc@gmail.com>
2023-05-10 14:21:25 -0300

    adding variables

---
+++ src/components/UI/variables.js
@@ -0,0 +1,23 @@
+export const primaryColor = "#2A7AE4";
+
+export const blackColorDark = "#000000E5";
+export const blackColorMedium = "#00000099";
+export const blackColorLight = "#00000080";
+export const blackColorLighter = "#9E9E9E";
+
+export const grayColorDark = "#C2C2C2";
+export const grayColorMedium = "#E5E5E5";
+export const grayColorLight = "#F5F5F5";
+
+export const courseColor1 = "#6BD1FF";
+export const courseColor2 = "#69953B";
+export const courseColor3 = "#FFBA05";
+export const courseColor4 = "#DC6EBE";
+export const courseColor5 = "#9CD33B";
+export const courseColor6 = "#6B5BE2";
+export const courseColor7 = "#FF8C2A";
+export const courseColor8 = "#9CD33B";
+
+export const errorColorDark = "#C62828";
+export const errorColorMedium = "#E53935";
+export const errorColorLight = "#FCE7E7";
