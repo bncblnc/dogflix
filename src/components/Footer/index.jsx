0b55f61cd1c6f1a1e1c7c1c93e43e7f6649f9c59
bncblnc <bncblnc@gmail.com>
2023-05-31 16:48:51 -0300

    adding links to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { MarginLine } from "../UI";
 
-import { grayColorLight } from "../UI/variables";
+import { TbBrandGithub as IconGithub } from "react-icons/tb";
+import { FiLinkedin as IconLinkedin } from "react-icons/fi";
+
+import { MarginLine } from "../UI";
+import { grayColorDark, primaryColor } from "../UI/variables";
 import LogoLink from "../LogoLink";
+import { Link } from "react-router-dom";
 
 const StyledFooter = styled.footer`
   width: 100%;
@@ -15,9 +19,38 @@ const StyledFooter = styled.footer`
   padding: 1.5rem;
 `;
 
-const TextFooter = styled.div`
-  color: ${grayColorLight};
+const Container = styled.div`
+  display: flex;
+  gap: 3rem;
+  align-items: center;
+`;
+
+const TextFooter = styled.p`
+  text-align: center;
+  color: ${grayColorDark};
   font-size: 1.2rem;
+  letter-spacing: 1px;
+  line-height: 1.2;
+  border-right: 2px solid ${grayColorDark};
+  padding-right: 3rem;
+`;
+
+const NavStyled = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const NavItem = styled(Link)`
+  color: ${grayColorDark};
+  font-size: 5rem;
+
+  transition: all 0.2s;
+
+  :hover {
+    color: ${primaryColor};
+    transform: scale(1.4);
+  }
 `;
 
 const Footer = () => {
@@ -26,7 +59,21 @@ const Footer = () => {
       <MarginLine />
       <StyledFooter>
         <LogoLink />
-        <TextFooter>Desenvolvido por Bianca Blanco </TextFooter>
+        <Container>
+          <TextFooter>
+            Desenvolvido por Bianca Blanco durante o <br />
+            programa "ONE - Oracle Next Education".{" "}
+          </TextFooter>
+
+          <NavStyled>
+            <NavItem to="https://www.linkedin.com/in/bncblnc/" target="_blank">
+              <IconLinkedin />
+            </NavItem>
+            <NavItem to="https://github.com/bncblnc" target="_blank">
+              <IconGithub />
+            </NavItem>
+          </NavStyled>
+        </Container>
       </StyledFooter>
     </>
   );
